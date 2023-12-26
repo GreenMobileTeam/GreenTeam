8de0ebd4fe7ea605e87eaf07e5768d9fb55ce36b
lsumin223 <sumin99223@gmail.com>
2023-12-26 16:12:26 +0900

    UI 추가 겸 스크립트 백업 (닉네임 추가)

--- Assets/Scripts/DB/UserManager.cs
+++ Assets/Scripts/DB/UserManager.cs
@@ -5,31 +5,34 @@ using UnityEngine.Networking;
 using UnityEngine.UI;
 using TMPro;
 
-
-public class Login : MonoBehaviour
+public class UserManager : MonoBehaviour
 {
+
     // Start is called before the first frame update
     public TMP_InputField usernameInput;
     public TMP_InputField passwordInput;
+    public TMP_InputField nicknameInput;
 
     private string serverURL = "localhost:3000"; // 서버 URL로 교체
 
+
     public void SignUp()
     {
-        StartCoroutine(SendSignUpRequest(usernameInput.text, passwordInput.text));
+        StartCoroutine(SendSignUpRequest(usernameInput.text, passwordInput.text, nicknameInput.text));
     }
 
     public void SignIn()
     {
-        StartCoroutine(SendSignInRequest(usernameInput.text, passwordInput.text));
+        StartCoroutine(SendLogInRequest(usernameInput.text, passwordInput.text));
     }
 
-    IEnumerator SendSignUpRequest(string username, string password)
+    IEnumerator SendSignUpRequest(string username, string password, string nickname)
     {
         string url = $"{serverURL}/signup";
         WWWForm form = new WWWForm();
         form.AddField("username", username);
         form.AddField("password", password);
+        form.AddField("nickname", nickname);
 
         using (UnityWebRequest request = UnityWebRequest.Post(url, form))
         {
@@ -46,7 +49,7 @@ public class Login : MonoBehaviour
         }
     }
 
-    IEnumerator SendSignInRequest(string username, string password)
+    IEnumerator SendLogInRequest(string username, string password)
     {
         string url = $"{serverURL}/login";
         WWWForm form = new WWWForm();
