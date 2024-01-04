439abfc8efc27c9195a5978ac826ce8a648de76b
lsumin223 <sumin99223@gmail.com>
2024-01-04 17:32:22 +0900

    로그아웃 오류 수정 아마도

--- Assets/Scripts/DB/SessionChecker.cs
+++ Assets/Scripts/DB/SessionChecker.cs
@@ -75,7 +75,7 @@ public class SessionChecker : MonoBehaviour
                 else if (response.message == "auto logout")
                 {
                     Debug.Log("다른 곳에서 로그인");
-                    SceneManager.LoadScene("login");
+                    SceneManager.LoadScene(0);
                 }
             }
             else
