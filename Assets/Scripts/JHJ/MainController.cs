affd6cdd70f0a94ab42e20dd9567e8ef6750ce91
J0Hyejin <141013135+J0Hyejin@users.noreply.github.com>
2024-01-04 16:33:53 +0900

    메인화면 추가랑 기타 버그 수정

---
+++ Assets/Scripts/JHJ/MainController.cs
@@ -0,0 +1,43 @@
+using UnityEngine.SceneManagement;
+using UnityEngine;
+using TMPro;
+using System.Collections;
+
+public class MainController : MonoBehaviour
+{
+    public TextMeshProUGUI text;
+    bool next = false;
+
+    private void Awake()
+    {
+        StartCoroutine(FadeTextToZero());
+    }
+
+    public IEnumerator FadeTextToFullAlpha() // 알파값 0에서 1로 전환
+    {
+        text.color = new Color(text.color.r, text.color.g, text.color.b, 0);
+        while (text.color.a < 1.0f)
+        {
+            text.color = new Color(text.color.r, text.color.g, text.color.b, text.color.a + (Time.deltaTime / 2.0f));
+            yield return null;
+        }
+        StartCoroutine(FadeTextToZero());
+    }
+
+    public IEnumerator FadeTextToZero()  // 알파값 1에서 0으로 전환
+    {
+        text.color = new Color(text.color.r, text.color.g, text.color.b, 1);
+        while (text.color.a > 0.0f)
+        {
+            text.color = new Color(text.color.r, text.color.g, text.color.b, text.color.a - (Time.deltaTime / 2.0f));
+            yield return null;
+        }
+        StartCoroutine(FadeTextToFullAlpha());
+    }
+
+    public void Touched()
+    {
+        next = true;
+        SceneManager.LoadScene("login");
+    }
+}
