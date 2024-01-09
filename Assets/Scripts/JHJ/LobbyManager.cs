e690f03aa631bae3b458f113b3cd5033e4507d1b
J0Hyejin <141013135+J0Hyejin@users.noreply.github.com>
2024-01-09 09:36:29 +0900

    대충 뭐 함

--- Assets/Scripts/JHJ/LobbyManager.cs
+++ Assets/Scripts/JHJ/LobbyManager.cs
@@ -107,15 +107,27 @@ public class LobbyManager : MonoBehaviourPunCallbacks
         if (PhotonNetwork.IsConnected)
         {
             Debug.Log("Connecting Room" +roomNum+"...");
-            Debug.Log(PlayerPrefs.GetString("Name"));
+            string n_ = PlayerPrefs.GetString("Nickname");
+            Debug.Log(PlayerPrefs.GetString("Nickname"));
             if (PlayerPrefs.GetInt("IsGuest") == 1)
-                PhotonNetwork.LocalPlayer.NickName = "Tester" + Random.Range(0, 101);
+            {
+                string n = "Tester" + Random.Range(0, 101);
+                PhotonNetwork.LocalPlayer.NickName = n;
+                PlayerPrefs.SetString("GuestName", n);
+            }
             else
             {
-                string[] temp = PlayerPrefs.GetString("Name").Split(":");
-                Debug.Log(temp[0]+ temp[1]);
-                string t = temp[1].Substring(1,temp[1].Length-3); 
-                PhotonNetwork.LocalPlayer.NickName = t;
+                if (n_ == "" || n_ == " ")
+                {
+                    Debug.Log("Name Error");
+                }
+                else
+                {
+                    string[] temp = n_.Split(":");
+                    Debug.Log(temp[0] + temp[1]);
+                    string t = temp[1].Substring(1, temp[1].Length - 3);
+                    PhotonNetwork.LocalPlayer.NickName = t;
+                }
             }
             //PhotonNetwork.JoinRandomRoom();
             PhotonNetwork.JoinOrCreateRoom("Map_" + roomNum, new RoomOptions { MaxPlayers = 10}, null);
